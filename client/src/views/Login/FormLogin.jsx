73840942657355bb6db94951647753c86d2b9465
unknown <javier17utn@gmail.com>
2024-05-20 20:06:57 -0300

    creacion de maqueta para vista login

---
+++ client/src/views/Login/FormLogin.jsx
@@ -0,0 +1,11 @@
+import style from "./login.module.css";
+
+const FormLogin = () => {
+  return (
+   <section className={style.formContainer}>
+    formulario login
+   </section>
+  )
+}
+
+export default FormLogin
