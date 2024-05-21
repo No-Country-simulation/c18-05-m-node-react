0ecdd1dc2e0489bee12838efc38adf2b8d15640c
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-05-21 11:12:53 -0300

    cambios de restauracion

---
+++ client/src/views/Login/CarruselLogin/CarruselLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from '../style.module.css'
+export const CarruselLogin = () => {
+    return (
+        <section className={style.Carrusel}>
+            Imagen
+        </section>
+    )
+}
