35d6b82a890e172a98d7b71706823c229f0baf77
Luis4nge1 <luis24.lmx@gmail.com>
2024-05-21 16:53:51 -0500

    Teacher

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,13 +3,18 @@ import StudentsModel from "./Students.js";
 import HistorialsModel from "./Historials.js";
 import { sequelize } from "../../db.js";
 import AdminModel from './Admin.js'
+import TeachersModel from './Teachers.js'
 
 const Admin = AdminModel(sequelize);
 const Students = StudentsModel(sequelize);
 const Historials = HistorialsModel(sequelize);
+const Teachers = TeachersModel(sequelize);
 
 // Definir relaciones aquí
 Students.hasOne(Historials, { foreignKey: "studentId", as: "historial" });
 Historials.belongsTo(Students, { foreignKey: "studentId", as: "student" });
 
-export { Students, Historials, Admin };
+Students.hasOne(Teachers, { foreignKey: "teacherId", as: "teacher" });
+Teachers.belongsTo(Students, { foreignKey: "teacherId", as: "student" });
+
+export { Students, Historials, Admin, Teachers };
