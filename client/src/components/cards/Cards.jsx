f577e480f709adc2b2fb084e80128aa7b52af507
unknown <javier17utn@gmail.com>
2024-05-23 23:55:23 -0300

    rutas dinamicas home y llamada api historias

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -3,7 +3,7 @@ import style from "./Cards.module.css";
 
 const Cards = ({informacion}) => {
   return (
-    <div className={style.cardsContainer}>
+    <div >
         {informacion.map((info) => (
             <Card
                 key={info.id}
---
+++ client/src/views/Home/components/HomeEstudiantes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeEstudiantes = () => {
+  return (
+    <div>HomeEstudiantes</div>
+  )
+}
+
+export default HomeEstudiantes
